90ad8142cda9bc8c2aa342d771f8a3504026b464
sergioivanov008 <sergioivanov008@gmail.com>
2024-05-06 13:47:28 +0300

    refactor: refactor vite.config.ts and dependencies

--- rs-commerce/vite.config.ts
+++ rs-commerce/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,32 +10,12 @@ export default defineConfig({
     globals: true,
     setupFiles: './setup.ts',
   },
+  define: {
+    global: {},
+  },
   resolve: {
     alias: {
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        // prettier-ignore
-        // eslint-disable-next-line
-        rollupNodePolyFill(),
-      ],
+      'node-fetch': 'isomorphic-fetch',
     },
   },
 });
