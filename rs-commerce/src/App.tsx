254db06fa864e80e6e4afde6ac2904e8ca447f3d
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-05-09 20:36:58 +0300

    feat: add pages layout

--- rs-commerce/src/App.tsx
+++ rs-commerce/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import routes from 'utils/routes';
+import Layout from 'components/layout/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.component} />
+          <Route key={route.path} path={route.path} element={<Layout>{route.component}</Layout>} />
         ))}
       </Routes>
     </BrowserRouter>
---
+++ rs-commerce/src/components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Footer from './Footer';
+import Header from './Header';
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header /> <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
