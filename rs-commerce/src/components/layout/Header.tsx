321e274c7f6a4647dea8572211d28ebe8d8cc2ce
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-05-10 22:29:36 +0300

    feat: add logout button

--- rs-commerce/src/components/layout/Header.tsx
+++ rs-commerce/src/components/layout/Header.tsx
@@ -1,11 +1,17 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Logo from 'assets/images/logo.png';
-import { ROUTE_PATH } from 'constants/constants';
+import { LOCAL_STORAGE_TOKEN, ROUTE_PATH } from 'constants/constants';
+import { useAppSelector } from 'hooks/typed-react-redux-hooks';
+import { apiAuthSlice } from 'redux/slices/api-auth-slice';
 import style from './style.module.css';
 
 function Header() {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const isAuth =
+    !!localStorage.getItem(LOCAL_STORAGE_TOKEN) && useAppSelector((state) => state.apiAuth.isAuth);
 
   const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
 
@@ -14,11 +20,13 @@ function Header() {
       path: ROUTE_PATH.login,
       title: 'Login',
       className: style['underline'],
+      withAuth: false,
     },
     {
       path: ROUTE_PATH.registration,
       title: 'Registration',
       className: style['underline'],
+      withAuth: false,
     },
   ];
 
@@ -27,6 +35,13 @@ function Header() {
   useEffect(() => {
     setIsBurgerMenuOpen(false);
   }, [location]);
+
+  const handleLogout = () => {
+    localStorage.removeItem(LOCAL_STORAGE_TOKEN);
+    apiAuthSlice.actions.resetApiAuthSlice();
+
+    navigate(ROUTE_PATH.main);
+  };
 
   return (
     <header className={`${style['header']} container`}>
@@ -45,13 +60,26 @@ function Header() {
 
       <nav className={`${style['nav']} ${isBurgerMenuOpen ? style['nav-open'] : ''}`}>
         <ul className={style['nav-right']}>
-          {rightList.map((item) => (
-            <li key={item.path}>
-              <Link to={item.path} className={item.className} aria-label={item.title}>
-                {item.title}
-              </Link>
+          {rightList
+            .filter((item) => (isAuth ? item.withAuth : true))
+            .map((item) => (
+              <li key={item.path}>
+                <Link to={item.path} className={item.className} aria-label={item.title}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          {isAuth && (
+            <li>
+              <button
+                aria-label="Logout"
+                onClick={handleLogout}
+                className={style['logout']}
+                type="button">
+                Logout
+              </button>
             </li>
-          ))}
+          )}
         </ul>
       </nav>
     </header>
