ec408e740616f2c3efc091e3956b17fb6ac7b2a0
Aliaksandr Fedukovich <1632637@mail.ru>
2024-05-15 14:13:13 +0300

    refact: create array for input elements

--- rs-commerce/src/views/registration/components/RegistrationForm/components/AddressRegistration/components/AddressComponents/AddressComponents.tsx
+++ rs-commerce/src/views/registration/components/RegistrationForm/components/AddressRegistration/components/AddressComponents/AddressComponents.tsx
@@ -5,47 +5,52 @@ import AddressTitleComponent, {
 } from './components/AddresTitleComponent/AddresTitleComponent';
 import classes from './style.module.css';
 
+let typeComponent: 'billing' | 'shipping' = 'billing';
+
+const inputFieldsArray = [
+  <InputRegistration
+    input={{
+      htmlFor: `${typeComponent}PostCode`,
+      title: 'Post code:',
+      type: 'text',
+      placeholder: 'your post code',
+      mistakeContent: 'error message',
+      smallSize: true,
+    }}
+  />,
+
+  <InputRegistration
+    input={{
+      htmlFor: `${typeComponent}City`,
+      title: 'City:',
+      type: 'text',
+      placeholder: 'your city',
+      mistakeContent: 'error message',
+      smallSize: true,
+    }}
+  />,
+
+  <InputRegistration
+    input={{
+      htmlFor: `${typeComponent}Street`,
+      title: 'Street:',
+      type: 'text',
+      placeholder: 'your street',
+      mistakeContent: 'error message',
+      smallSize: true,
+    }}
+  />,
+];
+
 function AddressComponent(props: AddressType) {
-  const { typeComponent } = props;
+  ({ typeComponent } = props);
   return (
     <div className={classes['registration__address-container']}>
       <AddressTitleComponent typeComponent={typeComponent} />
 
       <div className={classes['address']}>
         <CountryInput typeComponent={typeComponent} />
-
-        <InputRegistration
-          input={{
-            htmlFor: `${typeComponent}PostCode`,
-            title: 'Post code:',
-            type: 'text',
-            placeholder: 'your post code',
-            mistakeContent: 'error message',
-            smallSize: true,
-          }}
-        />
-
-        <InputRegistration
-          input={{
-            htmlFor: `${typeComponent}City`,
-            title: 'City:',
-            type: 'text',
-            placeholder: 'your city',
-            mistakeContent: 'error message',
-            smallSize: true,
-          }}
-        />
-
-        <InputRegistration
-          input={{
-            htmlFor: `${typeComponent}Street`,
-            title: 'Street:',
-            type: 'text',
-            placeholder: 'your street',
-            mistakeContent: 'error message',
-            smallSize: true,
-          }}
-        />
+        {inputFieldsArray.map((item) => item)}
       </div>
     </div>
   );
