61a99f2e1bd29042a81880588980712d692ebbc0
sergioivanov008 <sergioivanov008@gmail.com>
2024-05-16 01:20:39 +0300

    feat: iedirect users to the main page upon successful login

--- rs-commerce/src/views/login/Login.tsx
+++ rs-commerce/src/views/login/Login.tsx
@@ -1,7 +1,19 @@
 import LoginPageForm from 'components/login-page-form/login-page-form';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { ROUTE_PATH } from 'constants/constants';
+import { useAppSelector } from 'hooks/typed-react-redux-hooks';
+import { apiAuthSelector } from 'redux/selectors';
 import style from './style.module.css';
 
 function Login() {
+  const navigate = useNavigate();
+  const { isAuth } = useAppSelector(apiAuthSelector);
+
+  useEffect(() => {
+    if (isAuth) navigate(ROUTE_PATH.main);
+  }, [isAuth]);
+
   return (
     <div className={style['login-page']}>
       <div className={style['login-page-wrapper']}>
