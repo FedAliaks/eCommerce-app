9a59c41ebd0ed88e58fb388438f16e70f0a7ea4f
sergioivanov008 <sergioivanov008@gmail.com>
2024-05-24 13:58:17 +0300

    fix: fix FilterInfo values

--- rs-commerce/src/views/Catalog/components/FilterInfo.tsx
+++ rs-commerce/src/views/Catalog/components/FilterInfo.tsx
@@ -1,13 +1,15 @@
 import { useAppSelector } from 'hooks/typed-react-redux-hooks';
 import { apiCategoriesProductsSelector } from 'redux/selectors';
+import { NUMBER_ZERO } from 'constants/constants';
 import style from '../style.module.css';
 
 function FilterInfo(): JSX.Element {
   const { curProductsPage, productsInPage, productsTotal } = useAppSelector(
     apiCategoriesProductsSelector,
   );
-  const to = productsInPage * curProductsPage;
-  const from = productsInPage * (curProductsPage - 1);
+  const totalNumPages = productsTotal ? Math.ceil(productsTotal / productsInPage) : NUMBER_ZERO;
+  const from = productsInPage * (curProductsPage - 1) + 1;
+  const to = curProductsPage === totalNumPages ? productsTotal : productsInPage * curProductsPage;
 
   return (
     <div className={`${style['filter-view']} ${style['filter4']}`}>
