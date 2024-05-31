2bee069f5775a26de53f51b52a2dc63ae62b7736
sergioivanov008 <sergioivanov008@gmail.com>
2024-05-31 12:43:16 +0300

    fix: fix CategoryItem

--- rs-commerce/src/components/category-item/category-item.tsx
+++ rs-commerce/src/components/category-item/category-item.tsx
@@ -1,30 +1,31 @@
+import { useEffect } from 'react';
 import { CategoryItemProps } from 'types/types';
-import { useAppDispatch, useAppSelector } from 'hooks/typed-react-redux-hooks';
-import { apiCategoriesProductsSelector } from 'redux/selectors';
+import { useAppDispatch } from 'hooks/typed-react-redux-hooks';
 import { CATALOG_PAGE_TEXT, PAGE_NUMBER_ONE, ROUTE_PATH } from 'constants/constants';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { apiCategoriesProductsActions } from 'redux/slices/api-categories-products-slice';
 import style from './style.module.css';
 
 function CategoryItem({ data }: CategoryItemProps) {
   const dispatch = useAppDispatch();
-  const { curCategory } = useAppSelector(apiCategoriesProductsSelector);
+  const { category } = useParams();
   const title = data ? data.name['en'] : CATALOG_PAGE_TEXT.allCategories;
 
-  const curItemClick = (): void => {
-    dispatch(apiCategoriesProductsActions.setCurProductsPage(PAGE_NUMBER_ONE));
-    dispatch(apiCategoriesProductsActions.setCurCategory(data));
-  };
+  useEffect(() => {
+    if (category && data?.slug['en'] === category) {
+      dispatch(apiCategoriesProductsActions.setCurProductsPage(PAGE_NUMBER_ONE));
+      dispatch(apiCategoriesProductsActions.setCurCategory(data));
+    }
+  }, [category]);
 
   const curItemStyle = (): string =>
-    `${style['category-item']} ${curCategory?.name['en'] === data?.name['en'] ? style['active'] : ''}`;
+    `${style['category-item']} ${category === data?.slug['en'] ? style['active'] : ''}`;
 
   return (
     <Link
       to={`${ROUTE_PATH.catalog}${data?.slug['en'] ? `/${data?.slug['en']}` : ''}`}
       className={curItemStyle()}
-      role="presentation"
-      onClick={curItemClick}>
+      role="presentation">
       {title}
     </Link>
   );
