a77ce57fad9a602ef75affc13a7b2f558b61cea1
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-06-03 22:30:35 +0300

    fix: set isAuth if userData exists

--- rs-commerce/src/App.tsx
+++ rs-commerce/src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import routes from 'utils/routes';
 import Layout from 'components/layout/Layout';
 import { LOCAL_STORAGE_AUTH } from 'constants/constants';
-import { useAppDispatch } from 'hooks/typed-react-redux-hooks';
+import { useAppDispatch, useAppSelector } from 'hooks/typed-react-redux-hooks';
 import { apiAuthActions } from 'redux/slices/api-auth-slice';
+import { useEffect } from 'react';
+import { apiAuthSelector } from 'redux/selectors';
 
 function App() {
   const dispatch = useAppDispatch();
-  if (JSON.parse(localStorage.getItem(LOCAL_STORAGE_AUTH)!))
-    dispatch(apiAuthActions.setIsAuth(true));
+  const { userData } = useAppSelector(apiAuthSelector);
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem(LOCAL_STORAGE_AUTH)!) && userData)
+      dispatch(apiAuthActions.setIsAuth(true));
+  }, [userData]);
 
   return (
     <BrowserRouter>
